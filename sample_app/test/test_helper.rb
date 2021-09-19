69b7fe1eaeb72d9b5b1e47b671d32f2e2085c150
umihico <umihico@users.noreply.github.com>
2021-09-20 06:28:11 +0900

    feat: add tests

--- sample_app/test/test_helper.rb
+++ sample_app/test/test_helper.rb
@@ -7,7 +7,7 @@ Minitest::Reporters.use!
 class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
   parallelize(workers: :number_of_processors)
-
+  include ApplicationHelper
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
 
