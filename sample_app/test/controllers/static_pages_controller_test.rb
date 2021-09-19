086ffe8e6e3ccebb726a1527e8d9bf602ffdcfdd
umihico <umihico@users.noreply.github.com>
2021-09-20 06:36:55 +0900

    feat: modify path and add test

--- sample_app/test/controllers/static_pages_controller_test.rb
+++ sample_app/test/controllers/static_pages_controller_test.rb
@@ -34,4 +34,10 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
     assert_select 'title', "Contact | #{@base_title}"
   end
+
+  test 'should get signup' do
+    get signup_path
+    assert_response :success
+    assert_select 'title', "Signup | #{@base_title}"
+  end
 end
--- sample_app/test/controllers/users_controller_test.rb
+++ sample_app/test/controllers/users_controller_test.rb
@@ -1,9 +1,8 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
-    get users_new_url
+  test 'should get new' do
+    get signup_path
     assert_response :success
   end
-
 end
