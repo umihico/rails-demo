f4a0c6eb68e102b94e96d49ab1a35f5a2da1d35f
umihico <umihico@users.noreply.github.com>
2021-09-20 18:05:37 +0900

    feat: use ffaker to generate unique email

--- sample_app/test/integration/users_signup_test.rb
+++ sample_app/test/integration/users_signup_test.rb
@@ -15,7 +15,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     get signup_path
     assert_difference 'User.count', 1 do
       post users_path(params: { user: { name: 'Example User',
-                                        email: 'user@example.com',
+                                        email: FFaker::Internet.email,
                                         password: 'password',
                                         password_confirmation: 'password' } })
     end
