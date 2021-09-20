119684cc20be97283aa259b361dcf4120743ed3d
umihico <umihico@users.noreply.github.com>
2021-09-20 22:49:56 +0900

    fix

--- sample_app/db/seeds.rb
+++ sample_app/db/seeds.rb
@@ -6,8 +6,8 @@ User.create!(name: 'Example User',
 
 # 追加のユーザーをまとめて生成する
 99.times do |n|
-  name  = Faker::Name.name
   email = "example-#{n + 1}@railstutorial.org"
+  name = FFaker::Name.name
   password = 'password'
   User.create!(name: name,
                email: email,
